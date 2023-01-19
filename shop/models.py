bdf74f54c0696c2d8ed5257b290bebc92c534a53
goshva <sha.egor@gmail.com>
2023-01-19 20:49:23 +0300

    add null field and 50% img

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,7 @@ class images(models.Model):
     name=models.CharField(max_length=20)  
     loc=models.CharField(max_length=20)    
     image=models.ImageField(upload_to='images')   
+    
  
     def __str__(self):        
         return self.name
@@ -18,16 +19,16 @@ class Good(models.Model):
     text = models.TextField()
     kind = models.CharField(max_length=300)
     name = models.CharField(max_length=200)
-    photo_url = models.CharField(max_length=1000)
-    photo = images
+    photo_url = models.CharField(max_length=3000,null=True)
+    image= models.ImageField(upload_to='uploads/products/',null=True)
     article = models.CharField(max_length=20)
     color = models.CharField(max_length=250)
-    price_5k = models.FloatField()
-    price_10k = models.FloatField()
-    price_20k = models.FloatField()
-    price_many = models.FloatField()
-    size = models.FloatField()
-    weight = models.FloatField()
+    price_5k = models.FloatField(blank=True,null=True)
+    price_10k = models.FloatField(blank=True,null=True)
+    price_20k = models.FloatField(blank=True,null=True)
+    price_many = models.FloatField(blank=True,null=True)
+    size = models.FloatField(blank=True,null=True)
+    weight = models.FloatField(blank=True,null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
