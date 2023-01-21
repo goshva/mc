7c242a5304a94752a76bad5dbe8b3615a4a01be4
goshva <sha.egor@gmail.com>
2023-01-21 09:12:23 +0300

    add simple  shop stuff

--- README.md
+++
@@ -1,7 +0,0 @@
-python3 -m venv env
-
-source env/bin/activate
-
-pip install Django==4.1.5 
-
-python manage.py runserver
--- shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Good
-from .models import images   
-class AdminImages(admin.ModelAdmin):    
-    list_display = ['id_no','name','loc','image'] 
-
-admin.site.register(images,AdminImages)
-admin.site.register(Good)
