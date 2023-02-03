ea6b68d3a2e4ed8774ec42749c08a9458701c1b0
goshva <sha.egor@gmail.com>
2023-02-03 11:46:59 +0000

    server version of db

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.1.157','46.63.179.85']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.1.157','109.168.173.93']
 
 
 
